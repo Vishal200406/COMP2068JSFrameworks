740cd98d716e2b7e8d8399fdc637997169820fe2
Vishal200406 <200621192@student.georgianc.on.ca>
2026-06-17 13:01:44 +0300

    Configure portfolio routes and page views

--- Assignment01/routes/index.js
+++ Assignment01/routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const portfolio = {
+  name: 'Vishal Malhotra',
+  role: 'Computer Programming Student at Georgian College',
+  email: 'maliotravishal101@gmail.com',
+  github: 'https://github.com/Vishal200406',
+  linkedin: 'https://ca.linkedin.com/in/vishal-malhotra-953352417'
+};
+
+// Home page
+router.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Home',
+    isHome: true,
+    portfolio
+  });
+});
+
+// About Me page
+router.get('/about', function (req, res) {
+  res.render('about', {
+    title: 'About Me',
+    isAbout: true,
+    portfolio
+  });
+});
+
+// Projects page
+router.get('/projects', function (req, res) {
+  res.render('projects', {
+    title: 'Projects',
+    isProjects: true,
+    portfolio
+  });
+});
+
+// Contact Me page
+router.get('/contact', function (req, res) {
+  res.render('contact', {
+    title: 'Contact Me',
+    isContact: true,
+    portfolio
+  });
 });
 
 module.exports = router;
